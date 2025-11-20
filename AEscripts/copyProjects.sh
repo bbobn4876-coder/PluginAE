470b087b3134722f46e80dc0764c189eaa23c35b
bbobn4876-coder <bbobn4876@gmail.com>
2025-11-21 01:22:35 +0300

    Delete AEscripts directory

--- AEscripts/copyProjects.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp -R "/Library/Application Support/Adobe/CEP/extensions/com.motionland.bigpack/Projects/" /Users/$USER/Documents/AEviewer\ Packs/Installed\ Packs/Big\ Pack\ of\ the\ Elements/
-chmod -R 777 /Users/$USER/Documents/AEviewer\ Packs/Installed\ Packs/Big\ Pack\ of\ the\ Elements/

9bf5a36556a748e044ba8a36331cdb22e0f29c86
bbobn4876-coder <bbobn4876@gmail.com>
2025-11-20 03:22:02 +0300

    Add files via upload

---
+++ AEscripts/copyProjects.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp -R "/Library/Application Support/Adobe/CEP/extensions/com.motionland.bigpack/Projects/" /Users/$USER/Documents/AEviewer\ Packs/Installed\ Packs/Big\ Pack\ of\ the\ Elements/
+chmod -R 777 /Users/$USER/Documents/AEviewer\ Packs/Installed\ Packs/Big\ Pack\ of\ the\ Elements/
